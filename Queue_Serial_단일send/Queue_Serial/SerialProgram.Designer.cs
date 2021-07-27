df31791d29f8a2885d21c5c898edf167cf6e6532
seoyoungs <76426566+seoyoungs@users.noreply.github.com>
2021-07-27 18:07:17 +0900

    Add files via upload

---
+++ Queue_Serial_단일send/Queue_Serial/SerialProgram.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace Queue_Serial
+{
+    partial class SerialProgram
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn_PortSetting = new System.Windows.Forms.Button();
+            this.rtb_RecvText = new System.Windows.Forms.RichTextBox();
+            this.btn_Send = new System.Windows.Forms.Button();
+            this.tb_SendText = new System.Windows.Forms.TextBox();
+            this.btn_PortClose = new System.Windows.Forms.Button();
+            this.btn_PortOpen = new System.Windows.Forms.Button();
+            this.sp_Port = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // btn_PortSetting
+            // 
+            this.btn_PortSetting.Location = new System.Drawing.Point(409, 13);
+            this.btn_PortSetting.Name = "btn_PortSetting";
+            this.btn_PortSetting.Size = new System.Drawing.Size(75, 23);
+            this.btn_PortSetting.TabIndex = 26;
+            this.btn_PortSetting.Text = "포트설정";
+            this.btn_PortSetting.UseVisualStyleBackColor = true;
+            this.btn_PortSetting.Click += new System.EventHandler(this.btn_PortSetting_Click);
+            // 
+            // rtb_RecvText
+            // 
+            this.rtb_RecvText.Location = new System.Drawing.Point(20, 42);
+            this.rtb_RecvText.Name = "rtb_RecvText";
+            this.rtb_RecvText.ReadOnly = true;
+            this.rtb_RecvText.Size = new System.Drawing.Size(464, 225);
+            this.rtb_RecvText.TabIndex = 24;
+            this.rtb_RecvText.Text = "";
+            // 
+            // btn_Send
+            // 
+            this.btn_Send.Enabled = false;
+            this.btn_Send.Location = new System.Drawing.Point(18, 323);
+            this.btn_Send.Name = "btn_Send";
+            this.btn_Send.Size = new System.Drawing.Size(466, 61);
+            this.btn_Send.TabIndex = 23;
+            this.btn_Send.Text = "전송";
+            this.btn_Send.UseVisualStyleBackColor = true;
+            this.btn_Send.Click += new System.EventHandler(this.btn_Send_Click);
+            // 
+            // tb_SendText
+            // 
+            this.tb_SendText.Location = new System.Drawing.Point(19, 283);
+            this.tb_SendText.Name = "tb_SendText";
+            this.tb_SendText.Size = new System.Drawing.Size(463, 21);
+            this.tb_SendText.TabIndex = 22;
+            // 
+            // btn_PortClose
+            // 
+            this.btn_PortClose.Enabled = false;
+            this.btn_PortClose.Location = new System.Drawing.Point(115, 12);
+            this.btn_PortClose.Name = "btn_PortClose";
+            this.btn_PortClose.Size = new System.Drawing.Size(85, 24);
+            this.btn_PortClose.TabIndex = 21;
+            this.btn_PortClose.Text = "포트닫기";
+            this.btn_PortClose.UseVisualStyleBackColor = true;
+            this.btn_PortClose.Click += new System.EventHandler(this.btn_PortClose_Click);
+            // 
+            // btn_PortOpen
+            // 
+            this.btn_PortOpen.Location = new System.Drawing.Point(21, 12);
+            this.btn_PortOpen.Name = "btn_PortOpen";
+            this.btn_PortOpen.Size = new System.Drawing.Size(88, 24);
+            this.btn_PortOpen.TabIndex = 20;
+            this.btn_PortOpen.Text = "포트열기";
+            this.btn_PortOpen.UseVisualStyleBackColor = true;
+            this.btn_PortOpen.Click += new System.EventHandler(this.btn_PortOpen_Click);
+            // 
+            // sp_Port
+            // 
+            this.sp_Port.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.sp_Port_DataReceived);
+            // 
+            // SerialProgram
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 399);
+            this.Controls.Add(this.btn_PortSetting);
+            this.Controls.Add(this.rtb_RecvText);
+            this.Controls.Add(this.btn_Send);
+            this.Controls.Add(this.tb_SendText);
+            this.Controls.Add(this.btn_PortClose);
+            this.Controls.Add(this.btn_PortOpen);
+            this.Name = "SerialProgram";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_PortSetting;
+        private System.Windows.Forms.RichTextBox rtb_RecvText;
+        private System.Windows.Forms.Button btn_Send;
+        private System.Windows.Forms.TextBox tb_SendText;
+        private System.Windows.Forms.Button btn_PortClose;
+        private System.Windows.Forms.Button btn_PortOpen;
+        private System.IO.Ports.SerialPort sp_Port;
+    }
+}
+
---
+++ Queue_Serial_단일send/Queue_Serial/SettingPopup.Designer.cs
@@ -0,0 +1,232 @@
+﻿
+namespace Queue_Serial
+{
+    partial class SettingPopup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Cancel = new System.Windows.Forms.Button();
+            this.btn_Ok = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cb_StopBits = new System.Windows.Forms.ComboBox();
+            this.cb_DataBits = new System.Windows.Forms.ComboBox();
+            this.cb_Parity = new System.Windows.Forms.ComboBox();
+            this.cb_BaudRate = new System.Windows.Forms.ComboBox();
+            this.cb_PortNames = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_Cancel
+            // 
+            this.btn_Cancel.Location = new System.Drawing.Point(375, 187);
+            this.btn_Cancel.Name = "btn_Cancel";
+            this.btn_Cancel.Size = new System.Drawing.Size(85, 18);
+            this.btn_Cancel.TabIndex = 23;
+            this.btn_Cancel.Text = "Cancel";
+            this.btn_Cancel.UseVisualStyleBackColor = true;
+            this.btn_Cancel.Click += new System.EventHandler(this.btn_Cancel_Click);
+            // 
+            // btn_Ok
+            // 
+            this.btn_Ok.Location = new System.Drawing.Point(257, 187);
+            this.btn_Ok.Name = "btn_Ok";
+            this.btn_Ok.Size = new System.Drawing.Size(85, 18);
+            this.btn_Ok.TabIndex = 22;
+            this.btn_Ok.Text = "Ok";
+            this.btn_Ok.UseVisualStyleBackColor = true;
+            this.btn_Ok.Click += new System.EventHandler(this.btn_Ok_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "연결포트";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cb_StopBits);
+            this.groupBox1.Controls.Add(this.cb_DataBits);
+            this.groupBox1.Controls.Add(this.cb_Parity);
+            this.groupBox1.Controls.Add(this.cb_BaudRate);
+            this.groupBox1.Location = new System.Drawing.Point(12, 63);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(464, 104);
+            this.groupBox1.TabIndex = 21;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "포트설정";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(245, 69);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "정지비트";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(243, 17);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "데이터비트";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "패리티";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "통신속도";
+            // 
+            // cb_StopBits
+            // 
+            this.cb_StopBits.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_StopBits.FormattingEnabled = true;
+            this.cb_StopBits.Items.AddRange(new object[] {
+            "1",
+            "1.5",
+            "2"});
+            this.cb_StopBits.Location = new System.Drawing.Point(327, 69);
+            this.cb_StopBits.Name = "cb_StopBits";
+            this.cb_StopBits.Size = new System.Drawing.Size(121, 20);
+            this.cb_StopBits.TabIndex = 4;
+            // 
+            // cb_DataBits
+            // 
+            this.cb_DataBits.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_DataBits.FormattingEnabled = true;
+            this.cb_DataBits.Items.AddRange(new object[] {
+            "5",
+            "6",
+            "7",
+            "8"});
+            this.cb_DataBits.Location = new System.Drawing.Point(327, 14);
+            this.cb_DataBits.Name = "cb_DataBits";
+            this.cb_DataBits.Size = new System.Drawing.Size(121, 20);
+            this.cb_DataBits.TabIndex = 3;
+            // 
+            // cb_Parity
+            // 
+            this.cb_Parity.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_Parity.FormattingEnabled = true;
+            this.cb_Parity.Items.AddRange(new object[] {
+            "None",
+            "Odd",
+            "Even"});
+            this.cb_Parity.Location = new System.Drawing.Point(89, 69);
+            this.cb_Parity.Name = "cb_Parity";
+            this.cb_Parity.Size = new System.Drawing.Size(121, 20);
+            this.cb_Parity.TabIndex = 2;
+            // 
+            // cb_BaudRate
+            // 
+            this.cb_BaudRate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_BaudRate.FormattingEnabled = true;
+            this.cb_BaudRate.Items.AddRange(new object[] {
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "115200"});
+            this.cb_BaudRate.Location = new System.Drawing.Point(89, 14);
+            this.cb_BaudRate.Name = "cb_BaudRate";
+            this.cb_BaudRate.Size = new System.Drawing.Size(121, 20);
+            this.cb_BaudRate.TabIndex = 1;
+            // 
+            // cb_PortNames
+            // 
+            this.cb_PortNames.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_PortNames.FormattingEnabled = true;
+            this.cb_PortNames.Location = new System.Drawing.Point(69, 9);
+            this.cb_PortNames.Name = "cb_PortNames";
+            this.cb_PortNames.Size = new System.Drawing.Size(121, 20);
+            this.cb_PortNames.TabIndex = 19;
+            // 
+            // SettingPopup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 219);
+            this.Controls.Add(this.btn_Cancel);
+            this.Controls.Add(this.btn_Ok);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cb_PortNames);
+            this.Name = "SettingPopup";
+            this.Text = "Form2";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Cancel;
+        private System.Windows.Forms.Button btn_Ok;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cb_StopBits;
+        private System.Windows.Forms.ComboBox cb_DataBits;
+        private System.Windows.Forms.ComboBox cb_Parity;
+        private System.Windows.Forms.ComboBox cb_BaudRate;
+        private System.Windows.Forms.ComboBox cb_PortNames;
+    }
+}
